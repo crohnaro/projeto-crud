5eb12be569aaa5d34a68315e5629c827d9046719
crohnaro <viniciosalfa@hotmail.com>
2022-08-19 15:37:01 -0300

    Adicionado estrutura de rotas no server.js

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//definindo template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//definindo os arquivos public
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//habilita server receber dados via post
+app.use(express.urlencoded({extended: true}))
+
+//rotas
+app.get('/', (req, res) =>{
+    res.render('index', {
+        tittle: 'Titulo Teste'
+    })
+})
+
+
+//404 error (not found)
+app.use((req, res) =>{ //middleware
+    res.send('Pagina nao encontrada')
+})
+
+//executando o servidor para
+const port = process.env.PORT || 8080
+app.listen(port, ()=> console.log(`server is listening on ${port}`))
