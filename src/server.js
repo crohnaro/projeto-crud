d6ee9c63416a20a4482c54b3e00a1e00613000a2
crohnaro <viniciosalfa@hotmail.com>
2022-08-24 23:24:22 -0300

    Mudando estrutura de arquivos para as Routes ficarem escaláveis

--- src/server.js
+++ src/server.js
@@ -1,39 +1,31 @@
-const express = require('express');
-const path = require('path');
-
-const db = require('./database')
-
+const express = require("express");
+const path = require("path");
+const db = require("./database");
+const routes = require('./routes')
 
 const app = express();
 
-db.connect()
-
-
+db.connect();
 
 //definindo template engine
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //definindo os arquivos public
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //habilita server receber dados via post
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
-//rotas
-app.get('/', (req, res) =>{
-    res.render('index', {
-        tittle: 'Titulo Teste'
-    })
-})
-
+//definindo as rotas
+app.use('/', routes);
 
 //404 error (not found)
-app.use((req, res) =>{ //middleware
-    res.send('Pagina nao encontrada')
-})
+app.use((req, res) => {
+  //middleware
+  res.send("Pagina nao encontrada");
+});
 
 //executando o servidor para
-const port = process.env.PORT || 8080
-app.listen(port, ()=> console.log(`server is listening on ${port}`))
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`server is listening on ${port}`));
